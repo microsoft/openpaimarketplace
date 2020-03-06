31915a4d53458392e65a488fa239d7404a5c9c72
debuggy <mintao@microsoft.com>
2020-03-06 13:31:14 +0800

    Split description from json

---
+++ rest_server/src/controllers/database_error_handler.js
@@ -0,0 +1,18 @@
+const databaseErrorHandler = (error, res) => {
+  if (
+    error.name === "SequelizeConnectionRefusedError" ||
+    error.name === "SequelizeConnectionError"
+  ) {
+    res.status(500).send("database connection failed");
+  }
+  if (error.name === "SequelizeDatabaseError") {
+    res.status(500).send("database error");
+  } else {
+    throw error;
+  }
+};
+
+// module exports
+module.exports = {
+  databaseErrorHandler
+};
--- rest_server/src/controllers/item_controller.js
+++ rest_server/src/controllers/item_controller.js
@@ -2,6 +2,7 @@ const { Op } = require("sequelize");
 const { isNil } = require("lodash");
 const { MarketplaceItem } = require("../models");
 const asyncHandler = require("../asyncHandler");
+const { databaseErrorHandler } = require("./database_error_handler");
 
 const list = asyncHandler(async (req, res, next) => {
   try {
@@ -12,58 +13,68 @@ const list = asyncHandler(async (req, res, next) => {
     );
     res.status(200).json(result);
   } catch (e) {
-    if (
-      e.name === "SequelizeConnectionRefusedError" ||
-      e.name === "SequelizeConnectionError"
-    ) {
-      res.status(500).send("database connection failed");
-    }
-    if (e.name === "SequelizeDatabaseError") {
-      res.status(500).send("database error");
-    } else {
-      throw e;
-    }
+    databaseErrorHandler(e, res);
   }
 });
 
 const create = asyncHandler(async (req, res, next) => {
-  await MarketplaceItem.create(req.body);
-  res.status(201).send("created");
+  try {
+    await MarketplaceItem.create(req.body);
+    res.status(201).send("created");
+  } catch (e) {
+    databaseErrorHandler(e, res);
+  }
 });
 
 const get = asyncHandler(async (req, res, next) => {
-  const result = await MarketplaceItem.get(req.params.itemId);
-  if (isNil(result)) {
-    res.status(404).send("item not found");
-  } else {
-    res.status(200).json(result);
+  try {
+    const result = await MarketplaceItem.get(req.params.itemId);
+    if (isNil(result)) {
+      res.status(404).send("item not found");
+    } else {
+      res.status(200).json(result);
+    }
+  } catch (e) {
+    databaseErrorHandler(e, res);
   }
 });
 
 const update = asyncHandler(async (req, res, next) => {
-  const result = await MarketplaceItem.update(req.params.itemId, req.body);
-  if (isNil(result)) {
-    res.status(404).send("item not found");
-  } else {
-    res.status(200).send("updated");
+  try {
+    const result = await MarketplaceItem.update(req.params.itemId, req.body);
+    if (isNil(result)) {
+      res.status(404).send("item not found");
+    } else {
+      res.status(200).send("updated");
+    }
+  } catch (e) {
+    databaseErrorHandler(e, res);
   }
 });
 
 const del = asyncHandler(async (req, res, next) => {
-  const result = await MarketplaceItem.del(req.params.itemId);
-  if (isNil(result)) {
-    res.status(404).send("item not found");
-  } else {
-    res.status(200).send("deleted");
+  try {
+    const result = await MarketplaceItem.del(req.params.itemId);
+    if (isNil(result)) {
+      res.status(404).send("item not found");
+    } else {
+      res.status(200).send("deleted");
+    }
+  } catch (e) {
+    databaseErrorHandler(e, res);
   }
 });
 
 const listStarUsers = asyncHandler(async (req, res, next) => {
-  const users = await MarketplaceItem.listStarUsers(req.params.itemId);
-  if (isNil(users)) {
-    res.status(404).send("item not found");
-  } else {
-    res.status(200).json(users.map(user => user.name));
+  try {
+    const users = await MarketplaceItem.listStarUsers(req.params.itemId);
+    if (isNil(users)) {
+      res.status(404).send("item not found");
+    } else {
+      res.status(200).json(users.map(user => user.name));
+    }
+  } catch (e) {
+    databaseErrorHandler(e, res);
   }
 });
 
