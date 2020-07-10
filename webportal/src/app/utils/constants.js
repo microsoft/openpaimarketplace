d8b907de9e5fec9fdb856f07d5af6d4b40807bd4
debuggy <mintao@microsoft.com>
2020-07-10 13:29:23 +0800

    Update template

--- webportal/src/app/utils/constants.js
+++ webportal/src/app/utils/constants.js
@@ -15,7 +15,7 @@ export const MARKET_ITEM_LIST = [
     tags: ['official example'],
     summary: 'Dataset of couplet',
     description:
-      '# Couplet Dataset\n\nThis is the dataset of couplet. \n\n## Data content\n\nThis dataset contains processed data based on [Microsoft AI EDU project](https://github.com/microsoft/ai-edu/blob/master/B-%E5%AE%9E%E8%B7%B5%E6%A1%88%E4%BE%8B/B13-AI%E5%AF%B9%E8%81%94%E7%94%9F%E6%88%90%E6%A1%88%E4%BE%8B/docs/fairseq.md).\n\nThe original dataset was downloaded from [Public couplet dataset](https://github.com/wb14123/couplet-dataset) and was splited into ```test, train and valid``` with 98:1:1 proportion. The ```.up``` and ```.down``` files contains upper part and down part of a certain couplet seperately.\n\n## The file stucture\n\n```\n.\n|-- test.down // down part of couplet\n|-- test.up  // up part of couplet\n|-- train.down\n|-- train.up\n|-- valid.down\n|-- valid.up\n```\n\n## How to use it\n\nThe data was stored in a pai nfs storage. It will be mounted in container when you use the data in pai cluster.\n\n\n',
+      '# Couplet Dataset\n\nThis is the dataset of couplet. \n\n## Data content\n\nThis dataset contains processed data based on [Microsoft AI EDU project](https://github.com/microsoft/ai-edu/blob/master/B-%E5%AE%9E%E8%B7%B5%E6%A1%88%E4%BE%8B/B13-AI%E5%AF%B9%E8%81%94%E7%94%9F%E6%88%90%E6%A1%88%E4%BE%8B/docs/fairseq.md).\n\nThe original dataset was downloaded from [Public couplet dataset](https://github.com/wb14123/couplet-dataset) and was splited into ```test, train and valid``` with 98:1:1 proportion. The ```.up``` and ```.down``` files contains upper part and down part of a certain couplet seperately.\n\n## The file stucture\n\n```\n.\n|-- test.down // down part of couplet\n|-- test.up  // up part of couplet\n|-- train.down\n|-- train.up\n|-- valid.down\n|-- valid.up\n```\n\n## How to use it\n\nThe data will be mounted at ```DATA_DIR``` environment variable. You could use ```$DATA_DIR``` in your command when submit jobs in pai.\n\n\n',
     content: {
       dataStorage: {
         storageType: 'nfs',
@@ -33,14 +33,14 @@ export const MARKET_ITEM_LIST = [
   },
   {
     id: '8e0b3086-0359-4e75-b11c-c5527487626e',
-    name: 'Couplet Training Model',
+    name: 'Couplet Training Job Template',
     author: 'OpenPAI',
     type: 'template',
     categories: 'AI couplet',
     tags: ['official example'],
     summary: 'Dataset of couplet',
     description:
-      '# Couplet Training Model\n\nThis is a model training process. After training, this model will give a down part with an upper part of couplet. Please refer to [Microsoft AI Edu Project](https://github.com/microsoft/ai-edu/blob/master/B-%E5%AE%9E%E8%B7%B5%E6%A1%88%E4%BE%8B/B13-AI%E5%AF%B9%E8%81%94%E7%94%9F%E6%88%90%E6%A1%88%E4%BE%8B/docs/fairseq.md) for more details.\n\n## Training Data\n\nYou could use ```Couplet Dataset``` data component as training data, or any dataset follows ```fairseq``` model requirements.\n\n## How to use\n\nWhen use this module, you should set three environment variables:\n\n- ```RAW_DATA_DIR```: the training data path in container, if you use ```Couplet Dataset``` data component, this value will be auto filled.\n\n- ```PREPROCESSED_DATA_DIR```: the path to store intermediate result\n\n- ```MODEL_SAVE_DIR```: the path to store output result, i.e. the training model. You could use the predefined output storage, then you could get the results outside container.',
+      '# Couplet Training Model\n\nThis is a model training process. After training, this model will give a down part with an upper part of couplet. Please refer to Microsoft AI Edu Project for more details.\n\n## Training Data\n\nYou could use Couplet Dataset data component as training data, or any dataset follows fairseq model requirements.\n\n## How to use\n\nWhen use this module, you should set three environment variables:\n\n- ```DATA_DIR```: the training data path in container, by default it uses Couplet Dataset data component. If you want to use your own datasets. First make sure mount your data into container, and then change the ```$DATA_DIR``` with the path.\n\n- PREPROCESSED_DATA_DIR: the path to store intermediate result, by default it is ./processed_data.\n\n- ```OUTPUT_DIR```: the path to store output result, i.e. the training model files. By default it will mount a nfs storage, and you could change it with other mounted storage.\n\n## How to check the result\n\nAfter job finished successfully, you could check the output model files in the output storage. The storage server url is in details page.\n',
     content: {
       dockerImage: 'openpai/standard:python_3.6-pytorch_1.2.0-gpu',
       dataStorage: {
@@ -88,41 +88,37 @@ export const MARKET_ITEM_LIST = [
   },
   {
     id: 'a493d4cf-a79e-490f-95c9-06900cdcbd98',
-    name: 'Couplet Inference Model',
+    name: 'Couplet Inference Job Template',
     author: 'OpenPAI',
     type: 'template',
     categories: 'AI couplet',
     tags: ['official example'],
     summary: 'Dataset of couplet',
     description:
-      '# Caffe MNIST Example\nThis example shows how to train LeNet on MNIST with Caffe on OpenPAI.\n\n## Dataset\nThe MNIST dataset is downloaded from MNIST website and converted into caffe format.\n\n## LeNet\nThis example will use the LeNet network, which is known to work well on digit classification tasks.\nIt will use a slightly different version from the original LeNet implementation,\nreplacing the sigmoid activations with Rectified Linear Unit (ReLU) activations for the neurons.\n\nThe design of LeNet contains the essence of CNNs that are still used in larger models such as the ones in ImageNet.\nIn general, it consists of a convolutional layer followed by a pooling layer, another convolution layer followed by a pooling layer,\nand then two fully connected layers similar to the conventional multilayer perceptrons.\nThe layers are defined in `$CAFFE_ROOT/examples/mnist/lenet_train_test.prototxt`.\n\n## Reference\nhttp://caffe.berkeleyvision.org/gathered/examples/mnist.html\n',
+      '# Couplet Training Job Template\n\nThis is a model inference process. The input data is the trainning models trained by ```couplet training job```, and the this job will produce a url for user to ask for down part for a upper part of couplet.\n\n## How to use\n\nWhen use this module, you should set three environment variables:\n\n- ```DATA_DIR```: the training model path in container, by default it uses the output of couplet training job. If you want to use your own models. First make sure mount your models into container, and then change the ```$DATA_DIR``` with the path.\n\n- ```CODE_DIR```: the service code, it will start a server at the given port.\n\n- ```FLASK_RUN_PORT```: the service port container will output.\n\n## How to check the result\n\nAfter job finished successfully, you could check the job detail to get the container ip and ```flask_port``` number, then go to http://<ip>:<flask_port>/upper=<input> to test the result.\n',
     content: {
       dockerImage: 'openpai/standard:python_3.6-pytorch_1.2.0-gpu',
+      dataStorage: {
+        storageType: 'nfs',
+        groups: ['default'],
+        storageName: 'confignfs',
+        serverPath: '10.151.40.235:/data/couplet/checkpoints',
+        containerPath: '/mnt/confignfs/couplet/checkpoints',
+      },
       codeStorage: {
         storageType: 'nfs',
         groups: ['default'],
         storageName: 'confignfs',
-        serverPath: '10.151.40.235:/data/',
-        subPaths: 'couplet_service',
-        containerPath: '/mnt/confignfs/',
-      },
-      outputStorage: {
-        storageType: 'nfs',
-        groups: ['default'],
-        storageName: 'confignfs',
-        serverPath: '10.151.40.235:/data/',
-        subPaths: 'output',
-        containerPath: '/mnt/confignfs/',
-      },
-      environmentVariables: {
-        FLASK_RUN_PORT: { type: 'port', value: null },
+        serverPath: '10.151.40.235:/data/couplet',
+        containerPath: '/mnt/confignfs/couplet',
       },
       commands: [
-        ' pip install fairseq',
-        ' pip install flask',
-        ' cd /mnt/confignfs/couplet_service/',
-        ' export FLASK_APP=app.py',
-        ' python -m flask run',
+        'export FLASK_PORT=$PAI_PORT_LIST_taskrole_0_flask_port',
+        'pip install fairseq',
+        'pip install flask',
+        'pip install gunicorn',
+        'cd ${CODE_DIR}',
+        'gunicorn --bind=0.0.0.0:${FLASK_PORT} app:app',
       ],
     },
     useNumber: 0,
