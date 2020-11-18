66e23c97d19841cac75c91354abd08d74395c8f8
Xiang Long <mindsculptor@yeah.net>
2020-11-18 15:29:39 +0800

    Fix bug in npm install token loading

--- webportal/src/app/market_detail/utils/generate_job_protocol.js
+++ webportal/src/app/market_detail/utils/generate_job_protocol.js
@@ -2,14 +2,14 @@ import { getConnectionString } from 'App/utils/marketplace_api';
 
 export async function generateJobProtocol(item, user) {
   const connectionString = await getConnectionString(); // can add user as key to get connection string
-  const npmInstllToken = process.env.NPM_INSTLL_TOKEN;
+  const npmInstallToken = process.env.NPM_INSTALL_TOKEN;
   const protocolHeaderArray = [
     'curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh',
     'bash nodesource_setup.sh',
     'apt update',
     'apt install -y nodejs',
     'npm config set @swordfaith:registry https://npm.pkg.github.com/',
-    `echo "//npm.pkg.github.com/:_authToken=${npmInstllToken}" >> ~/.npmrc`,
+    `echo "//npm.pkg.github.com/:_authToken=${npmInstallToken}" >> ~/.npmrc`,
     'npm install -g @swordfaith/pai_copy',
     `export STORAGE_CONNECTION_STRING="${connectionString}"`,
   ];
