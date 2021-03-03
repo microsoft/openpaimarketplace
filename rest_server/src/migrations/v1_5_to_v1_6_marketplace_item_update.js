518c76d5078a0ea6025a75c209eec1ac30f813ed
Xiang Long <mindsculptor@yeah.net>
2021-03-03 15:52:41 +0800

    Add sequelize cli

---
+++ rest_server/src/migrations/v1_5_to_v1_6_marketplace_item_update.js
@@ -0,0 +1,8 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // logic for transforming into the new state
+  },
+  down: (queryInterface, Sequelize) => {
+    // logic for reverting the changes
+  }
+}
