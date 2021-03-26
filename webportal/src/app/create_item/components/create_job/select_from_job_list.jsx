2927721c465959de1dc4c2075a96a156c4ae5376
Yi Yi <yiyi@microsoft.com>
2021-03-26 17:01:18 +0800

    update

--- webportal/src/app/create_item/components/create_job/select_from_job_list.jsx
+++ webportal/src/app/create_item/components/create_job/select_from_job_list.jsx
@@ -3,10 +3,58 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text } from 'office-ui-fabric-react';
+import {
+  Text,
+  Stack,
+  DefaultButton,
+  PrimaryButton,
+} from 'office-ui-fabric-react';
+import { isEmpty } from 'lodash';
 
 const SelectFromJobList = ({ state, setState }) => {
-  return <Text>Job list here.</Text>;
+  return (
+    <>
+      <Text>Job list here.</Text>
+      <Stack
+        horizontal
+        horizontalAlign='space-between'
+        gap='l1'
+        styles={{
+          root: {
+            marginTop: '10px',
+          },
+        }}
+      >
+        <DefaultButton
+          text='Back'
+          onClick={() => {
+            setState({ step: 'uploadFiles' });
+          }}
+        />
+        <PrimaryButton
+          text='Next'
+          onClick={() => {
+            if (
+              isEmpty(state.itemObject.name) ||
+              isEmpty(state.itemObject.type) ||
+              isEmpty(state.itemObject.summary) ||
+              isEmpty(state.itemDescription.description)
+            ) {
+              setErrorMessage(true);
+              alert('please enter all required fields.');
+            } else {
+              setErrorMessage(false);
+              state.itemObject.description = generateDescription(state);
+              setState({
+                step: 'detail',
+                itemObject: state.itemObject,
+              });
+            }
+          }}
+        />
+      </Stack>
+    </>
+  );
 };
 
 SelectFromJobList.propTypes = {
