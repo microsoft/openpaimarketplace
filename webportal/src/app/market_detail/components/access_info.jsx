a21821ca6de6fd6eb179cc5965b2d0e2c289ec01
Xiang Long <mindsculptor@yeah.net>
2021-03-30 09:58:36 +0800

    Add access info component and display in market_detail page (#268)
    * Update time info from create time to update time in marketplace
    * Add AccessInfo Components and display in market_detail

---
+++ webportal/src/app/market_detail/components/access_info.jsx
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { Stack, Text, TooltipHost } from 'office-ui-fabric-react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const AccessInfo = props => {
+  const { isPublic, isPrivate, groupList } = props;
+  const groupListInfo = groupList.toString();
+
+  return (
+    <TooltipHost
+      calloutProps={{
+        isBeakVisible: false,
+      }}
+      delay={0}
+      tooltipProps={{
+        onRenderContent: () => {
+          return (
+            <Stack gap='m'>
+              <Text>Access Info</Text>
+              {isPublic === false && isPrivate === false && (
+                <Stack gap='s1'>
+                  <Text variant={'small'}>Share-to groups:</Text>
+                  <Text>{groupListInfo}</Text>
+                </Stack>
+              )}
+            </Stack>
+          );
+        },
+      }}
+    >
+      <Stack horizontal gap='s1'>
+        {isPublic === true && <Text>Public</Text>}
+        {isPublic === false && isPrivate === true && <Text>Private</Text>}
+        {isPublic === false && isPrivate === false && <Text>Protected</Text>}
+      </Stack>
+    </TooltipHost>
+  );
+};
+
+AccessInfo.propTypes = {
+  isPublic: PropTypes.bool,
+  isPrivate: PropTypes.bool,
+  groupList: PropTypes.arrayOf(PropTypes.string),
+};
