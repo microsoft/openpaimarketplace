0c3818ef5772c189309a784d7a6e883bff53713d
Yi Yi <yiyi@microsoft.com>
2021-04-09 15:41:33 +0800

    remove required from short summary and description

--- webportal/src/app/create_item/components/create_job/basic_information.jsx
+++ webportal/src/app/create_item/components/create_job/basic_information.jsx
@@ -102,7 +102,6 @@ const BasicInformation = props => {
         <TextField
           label='Short summary'
           description='(No more than 100 characters)'
-          required
           value={state.itemObject.summary}
           onChange={(event, newValue) => {
             const itemObject = cloneDeep(state.itemObject);
@@ -118,7 +117,6 @@ const BasicInformation = props => {
         />
         <TextField
           label='Description'
-          required
           multiline={true}
           rows={2}
           value={state.itemDescription.description}
@@ -236,12 +234,7 @@ const BasicInformation = props => {
         <PrimaryButton
           text='Next'
           onClick={() => {
-            if (
-              isEmpty(state.itemObject.name) ||
-              isEmpty(state.itemObject.type) ||
-              isEmpty(state.itemObject.summary) ||
-              isEmpty(state.itemDescription.description)
-            ) {
+            if (isEmpty(state.itemObject.name) || isEmpty(state.itemObject.type)) {
               setErrorMessage(true);
               alert('please enter all required fields.');
             } else {
